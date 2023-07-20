718bafab0a27bb15f9f3725a770f84a8d1608275
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-07-20 17:31:36 +1000

    Updating Custom User string

--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -7,6 +7,5 @@ class CustomUser(AbstractUser):
         """
         Return the first_name plus the last_name, with a space in between.
         """
-        full_name = "%s %s" % (self.first_name, self.last_name)
-        return full_name.strip()
-    
+        full_name = AbstractUser.get_full_name(self)
+        return full_name
