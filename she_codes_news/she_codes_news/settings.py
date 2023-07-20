2631898d87894f563808faba9a95c0e538dd8562
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-07-20 17:32:49 +1000

    Updating reset password views

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -33,13 +33,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'news.apps.NewsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
+    
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
