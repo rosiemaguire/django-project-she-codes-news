83a8bebda2626beedd04d820efc1ff89a8d5a129
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-07-22 15:22:04 +1000

    Continuing work on search functionality

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -3,7 +3,9 @@ from django.views import generic
 from django.urls import reverse_lazy
 from .models import NewsStory
 from .forms import StoryForm
-from django.http import HttpResponseRedirect
+from django.db.models import Q
+from users.models import CustomUser
+
 
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
@@ -16,7 +18,6 @@ class IndexView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_stories'] = NewsStory.objects.all().order_by("-pub_date")[:4]
-        # context['stories_by_author'] = NewsStory.objects.filter(author__contains = self.request.user.id).order_by("-pub_date")
         return context
 
 class StoryView(generic.DetailView):
@@ -35,32 +36,17 @@ class AddStoryView(generic.CreateView):
         return super().form_valid(form)
 
 class SearchFeature(generic.ListView):
-    form_class = StoryForm
+    model = NewsStory
     template_name = 'news/searchStory.html'
     context_object_name = 'searchView'
-    # success_url = reverse_lazy('news:searchView')
-    
-    def get_queryset(self):
-        '''Return all news stories.'''
-        
-        return NewsStory.objects.all().order_by("author")
-    #
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_stories'] = NewsStory.objects.all().order_by("-pub_date")[:4]
-        # context['stories_by_author'] = NewsStory.objects.filter(author_id=self.request.user.id)
-                                                                # = self.request.user.id).order_by("-pub_date")
         return context
 
     def post(self,request):
-        # Check if the request is a post request.
-        # if request.method == 'POST':
-            # Retrieve the search query entered by the user
-            search_query = request.POST['article_search']
-            # Filter your model by the search query
-            stories_by_author = NewsStory.objects.filter(title__contains=search_query)
-            return render(request, 'news/searchStory.html', {'query': search_query,'posts':stories_by_author})
-        # else:
-        #     return render(request, 'news/index.html',{})
-        
+        search_query = request.POST['article_search']
+        search_results = NewsStory.objects.filter(Q(title__icontains=search_query) | Q(content__icontains=search_query))
+        return render(request, 'news/searchStory.html', {'query': search_query,'search_results':search_results})
+
